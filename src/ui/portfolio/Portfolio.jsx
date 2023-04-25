51d80876b398d18b639fc8ab112431f6519ae616
Adrian Albescu <0xyoyobojo@gmail.com>
2023-04-24 23:03:40 -0400

    added new companies

--- src/ui/portfolio/Portfolio.jsx
+++ src/ui/portfolio/Portfolio.jsx
@@ -1,106 +1,17 @@
 import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import { PORTFOLIO_ITEMS } from "../../utils/companies";
 import PortfolioModalItem from "./modal";
 
 const Portfolio = () => {
-  const PORTFOLIO_ITEMS = [
-    {
-      company: "zeroDAO",
-      link: "zerodao.com",
-      img: "/img/companies/ZD-logo-3d.png",
-      preview: "/img/companies/zerodao-preview.png",
-      languages: "NextTS, Solidity, Typescript, SQL",
-      description: "zeroDAO is the decentralized governor of the ZERO network, enabling cross-chain messaging for trade or even more complex scripting.",
-      type: "Blockchain"
-    },
-    {
-      company: "VMEX Finance",
-      link: "vmex.finance",
-      img: "/img/companies/vmex-logo.svg",
-      preview: "/img/companies/vmex-preview.png",
-      languages: "NextTS, Solidity, Typescript, SQL",
-      description: "VMEX Finance is a decentralized, next-generation lending and borrowing protocol.",
-      type: "Blockchain"
-    },
-    {
-      company: "DataEarn",
-      link: "app.dataearn.com",
-      img: "/img/companies/dataearn-logo(1).png",
-      preview: "/img/companies/dataearn-preview.png",
-      languages: "NextTS, Python, Django, SQL",
-      description: "DataEarn is building a way to allow individuals to access, understand, protect, and monetize their data.",
-      type: "Web App",
-      padding: 'p-6'
-    },
-    {
-      company: "Raycon",
-      link: "rayconglobal.com",
-      img: "/img/companies/raycon-logo(1).png",
-      preview: "/img/companies/raycon-screenshot.png",
-      languages: "Javascript, React, Shopify, HTML, CSS",
-      description: "Raycon is an innovative sound system company, specializing in headphones.",
-      type: "Website",
-      padding: 'p-4'
-    },
-    {
-      company: "Vita Films",
-      link: "vitafilmllc.com",
-      img: "/img/companies/vita-logo.png",
-      preview: "/img/companies/vita-preview.png",
-      languages: "Nextjs, CSS",
-      description: "Vita Film is a videography and media company specializing in outdoor, wedding, and commercial spaces.",
-      type: "Website",
-      padding: 'p-4'
-    },
-    {
-      company: "Bonanza Exorsa",
-      link: "bonanzaexorsa.com",
-      img: "/img/companies/bonanza-3d-logo.png",
-      preview: "/img/companies/bonanza-preview.png",
-      languages: "NextTS, Solidity, Typescript",
-      description: "Bonanza Exorsa is a metaphysical brand using NFT's.",
-      type: "Blockchain",
-      padding: 'p-4'
-    },
-    {
-      company: "Live Duel",
-      link: "liveduel.com",
-      img: "/img/companies/liveduel-logo.png",
-      preview: "/img/companies/liveduel-preview.png",
-      languages: "NextTS, Solidity, Typescript, SQL",
-      description: "LiveDuel combines live streaming and interactive content like our prediction exchange and live trivia game show using blockchain technology.",
-      type: "Blockchain",
-      padding: 'p-2'
-    },
-    {
-      company: "Sports Roulette ATL",
-      link: "sportsrouletteatl.com",
-      img: "/img/companies/sra-logo.png",
-      preview: "/img/companies/sra-preview.png",
-      languages: "NextTS, CSS",
-      description: "Sports Roulette ATL is a fun sports league for adults looking to casually network.",
-      type: "Website"
-    },
-    {
-      company: "Lazar Stucco",
-      link: "lazarstucco.com",
-      img: "/img/companies/lazar-logo.png",
-      preview: "/img/companies/lazar-preview.png",
-      languages: "HTML, CSS",
-      description: "Lazar Stucco is an experienced contractor working with European Stucco for residential and commercial construction. ",
-      type: "Website",
-      padding: 'p-4'
-    }
-  ]
-
   return (
     <Tabs>
       <TabList className="portfolio-tab-list">
         <Tab>ALL</Tab>
         <Tab>BLOCKCHAIN</Tab>
         <Tab>WEBSITES</Tab>
-        {/* <Tab>ECOMMERCE</Tab>
-        <Tab>MOBILE</Tab> */}
+        <Tab>ECOMMERCE</Tab>
+        {/* <Tab>MOBILE</Tab> */}
       </TabList>
 
       <div className="portfolio-tab-content">
---
+++ src/utils/companies.js
@@ -0,0 +1,129 @@
+export const PORTFOLIO_ITEMS = [
+    {
+      company: "VMEX Finance",
+      link: "vmex.finance",
+      img: "/img/companies/vmex-logo.svg",
+      preview: "/img/companies/vmex-preview.png",
+      languages: "NextTS, Solidity, Typescript, SQL",
+      description: "VMEX Finance provides decentralized, next-generation liquidity markets.",
+      type: "Blockchain"
+    },
+    {
+      company: "Raycon",
+      link: "rayconglobal.com",
+      img: "/img/companies/raycon-logo(1).png",
+      preview: "/img/companies/raycon-screenshot.png",
+      languages: "Javascript, React, Shopify, HTML, CSS",
+      description: "Raycon is an innovative sound system company, specializing in headphones.",
+      type: "Ecommerce",
+      padding: 'p-4'
+    },
+    {
+      company: "zeroDAO",
+      link: "zerodao.com",
+      img: "/img/companies/ZD-logo-3d.png",
+      preview: "/img/companies/zerodao-preview.png",
+      languages: "NextTS, Solidity, Typescript, SQL",
+      description: "zeroDAO is the decentralized governor of the ZERO network, enabling cross-chain messaging for trade or even more complex scripting.",
+      type: "Blockchain"
+    },
+    {
+        company: "Papes",
+        link: "papes.xyz",
+        img: "/img/companies/papes-logo.svg",
+        preview: "/img/companies/papes-preview.png",
+        languages: "NextJS, Solidity, Typescript",
+        description: "Papes XYZ is a premium Bored Apes Yacht Club exclusive brand selling rolling papers.",
+        type: "Blockchain",
+        padding: 'p-0'
+      },
+    {
+      company: "DataEarn",
+      link: "app.dataearn.com",
+      img: "/img/companies/dataearn-logo(1).png",
+      preview: "/img/companies/dataearn-preview.png",
+      languages: "NextTS, Python, Django, SQL",
+      description: "DataEarn is building a way to allow individuals to access, understand, protect, and monetize their data.",
+      type: "Web App",
+      padding: 'p-6'
+    },
+    {
+      company: "Vita Films",
+      link: "vitafilmllc.com",
+      img: "/img/companies/vita-logo.png",
+      preview: "/img/companies/vita-preview.png",
+      languages: "Nextjs, CSS",
+      description: "Vita Film is a videography and media company specializing in outdoor, wedding, and commercial spaces.",
+      type: "Website",
+      padding: 'p-4'
+    },
+    {
+        company: "PintSwap",
+        link: "pintswap.eth.limo",
+        img: "/img/companies/pintswap-logo.png",
+        preview: "/img/companies/pintswap-preview.png",
+        languages: "NextJS, LibP2P, Solidity, Typescript",
+        description: "PintSwap is a peer-to-peer decentralized exchange using multi-party transaction scripts.",
+        type: "Blockchain",
+        padding: 'p-4'
+    },
+    {
+      company: "Bonanza Exorsa",
+      link: "bonanzaexorsa.com",
+      img: "/img/companies/bonanza-3d-logo.png",
+      preview: "/img/companies/bonanza-preview.png",
+      languages: "NextTS, Solidity, Typescript",
+      description: "Bonanza Exorsa is a metaphysical brand using NFT's.",
+      type: "Blockchain",
+      padding: 'p-4'
+    },
+    {
+        company: "Marmolaz",
+        link: "marmolaz.com",
+        img: "/img/companies/marmolaz-logo.png",
+        preview: "/img/companies/marmolaz-preview.png",
+        languages: "Shopify",
+        description: "Marmolaz creates natural, lime-based, construction products.",
+        type: "Ecommerce",
+        padding: 'p-4'
+    },
+    {
+      company: "LiveDuel",
+      link: "liveduel.com",
+      img: "/img/companies/liveduel-logo.png",
+      preview: "/img/companies/liveduel-preview.png",
+      languages: "NextTS, Solidity, Typescript, SQL",
+      description: "LiveDuel combines live streaming and interactive content like our prediction exchange and live trivia game show using blockchain technology.",
+      type: "Blockchain",
+      padding: 'p-2'
+    },
+    {
+      company: "Lazar Stucco",
+      link: "lazarstucco.com",
+      img: "/img/companies/lazar-logo.png",
+      preview: "/img/companies/lazar-preview.png",
+      languages: "HTML, CSS",
+      description: "Lazar Stucco is an experienced contractor working with European Stucco for residential and commercial construction. ",
+      type: "Website",
+      padding: 'p-4'
+    },
+    {
+        company: "Fellowship Event Hall",
+        link: "events.saintmaryro.org",
+        img: "/img/companies/fellowship-logo.png",
+        preview: "/img/companies/fellowship-preview.png",
+        languages: "NextJS, SendGrid, Typescript",
+        description: "Fellowship Event Hall is an event hall in Georgia hosting various celebrations and activities.",
+        type: "Website",
+        padding: 'p-4'
+      },
+    {
+      company: "Sports Roulette ATL",
+      link: "sportsrouletteatl.com",
+      img: "/img/companies/sra-logo.png",
+      preview: "/img/companies/sra-preview.png",
+      languages: "NextJS, TailwindCSS, Typescript",
+      description: "Sports Roulette ATL is a fun sports league for adults looking to casually network.",
+      type: "Website"
+    },
+  ]
